42833febf4d0777031d7e974c2e93b10319b0ed5
Atsushi Mori <m-atsushi@freemind.co.jp>
2022-11-11 17:10:29 +0900

    pecoコマンドの指定を修正

--- functions/peco_select_git_show.fish
+++ functions/peco_select_git_show.fish
@@ -2,6 +2,7 @@ function peco_select_git_show
   if set -q $argv
     git log --oneline | peco | cut -d " " -f1 | read line; git show $line
   else
-    git log --oneline | peco --query $argv | cut -d " " -f1 | read line; git show $line
+    set peco_flags --query "$argv"
+    git log --oneline | peco $peco_flags | cut -d " " -f1 | read line; git show $line
   end
 end
